3d35b6d048ee039ed4d3fb8d51050b8b8c4eddcd
Jack Warren <jack@jackwarren.info>
2019-05-15 11:30:25 -0400

    parsed_date.dart for date parsing

---
+++ lib/src/date/parsed_date.dart
@@ -0,0 +1,58 @@
+import 'dart:core';
+
+/// Immutable value class for the components of a date.
+class ParsedDate {
+  int _year, _month, _day;
+
+  /// Get the date's year.
+  int get year => _year;
+
+  /// Get the date's month.
+  int get month => _month;
+
+  /// Get the date's day.
+  int get day => _day;
+
+  /// Constructor for [ParsedDate], where if the [year] is null it is stored as 0 but [month] and
+  /// [day] are stored as is (null if they aren't provided).
+  ParsedDate(int year, [int month, int day]) {
+    _year = year ?? 0;
+    _month = month;
+    _day = day;
+  }
+
+  /// Static method constructor for [ParsedDate] so that this class may be constructed via
+  /// reference.
+  static ParsedDate constantConstructor(int year, [int month, int day]) =>
+      ParsedDate(year, month, day);
+
+  /// Turn this [ParsedDate] into a Dart [DateTime], using 1 in place of a null [month] or [day].
+  DateTime toDateTime() => DateTime(year, month ?? 1, day ?? 1);
+
+  /// Evaluate if the [other] object is the same as this, by type and field equality.
+  @override
+  bool operator ==(other) =>
+      other is ParsedDate && year == other.year && month == other.month && day == other.day;
+
+  /// Alias [toString] to [diagnosticString], forcing default parameters.
+  @override
+  String toString() => diagnosticString();
+
+  /// Print out the [day], [month], and [year] (in that order) with labels before each term.
+  ///
+  /// If a term is null, both the label and the null will be omitted. Since [year] cannot be
+  /// null, it and its label will always be present.
+  String diagnosticString(
+          {String separator = ' ',
+          String dayLabel = '[Day]:',
+          String monthLabel = '[Month]:',
+          String yearLabel = '[Year]:'}) =>
+      [
+        if (day != null) dayLabel,
+        if (day != null) day,
+        if (month != null) monthLabel,
+        if (month != null) month,
+        yearLabel,
+        year
+      ].join(separator);
+}
---
+++ test/date_test.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:test/test.dart';
+import 'package:best_effort_parser/src/date/parsed_date.dart';
+
+main() {
+  group('ParsedDate', () {
+    group('operator ==', () {
+      test('returns false if given a different type', () {
+        expect(ParsedDate(2000) == 5, false);
+      });
+
+      test('correctly evaluates fields', () {
+        final samples = [
+          ParsedDate(2000),
+          ParsedDate(1000),
+          ParsedDate(2000, 10),
+          ParsedDate(2000, 5),
+          ParsedDate(2000, 10, 10),
+          ParsedDate(2000, 10, 5)
+        ];
+        for (int a = 0; a < samples.length; a++)
+          for (int b = 0; b < samples.length; b++) expect(samples[a] == samples[b], a == b);
+      });
+    });
+
+    group('toDateTime', () {
+      test('creates the expected DateTime', () {
+        final r = Random();
+        for (int i = 0; i < 3; i++) {
+          int y = r.nextInt(2500), m = r.nextInt(12), d = r.nextInt(28);
+          expect(ParsedDate(y).toDateTime(), DateTime(y));
+          expect(ParsedDate(y, m).toDateTime(), DateTime(y, m));
+          expect(ParsedDate(y, m, d).toDateTime(), DateTime(y, m, d));
+        }
+      });
+    });
+
+    group('toString', () {
+      test('is the same as diagnosticString', () {
+        final r = Random();
+        for (int i = 0; i < 3; i++) {
+          var result = ParsedDate(r.nextInt(2500), r.nextInt(12), r.nextInt(28));
+          expect(result.toString(), result.diagnosticString());
+        }
+      });
+    });
+
+    group('diagnosticString', () {
+      test('only includes values when it should', () {
+        expect(ParsedDate(2000).diagnosticString(), '[Year]: 2000');
+        expect(ParsedDate(2000, 10).diagnosticString(), '[Month]: 10 [Year]: 2000');
+        expect(ParsedDate(2000, 10, 10).diagnosticString(), '[Day]: 10 [Month]: 10 [Year]: 2000');
+      });
+
+      test('works with optional parameters', () {
+        expect(
+            ParsedDate(1, 1, 1)
+                .diagnosticString(separator: '', dayLabel: '', monthLabel: '.', yearLabel: '.'),
+            '1.1.1');
+      });
+    });
+  });
+}
