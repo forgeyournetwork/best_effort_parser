abf00e71393a4611ccfffd764be6dc575ff6886f
Jack Warren <jack@jackwarren.info>
2019-05-17 21:51:44 -0400

    example of using DateParser

---
+++ example/date_example.dart
@@ -0,0 +1,11 @@
+import 'package:best_effort_parser/date.dart';
+
+/// Bare-bones example of using [DateParser] as a command-line program. All arguments are
+/// concatenated with spaces and then run through the parser with default settings.
+///
+/// Example usage:
+/// ```bash
+/// $ dart date_example.dart 'January 1st, 2019'
+/// [Day]: 1 [Month]: 1 [Year]: 2019
+/// ```
+void main(List<String> arguments) => DateParser.basic().parse(arguments.join(' ')).forEach(print);
